2a60a7ebdfcbd5b3bca7b559393ccdfc088518ae
Tomasz_Debosz <tomasz.debosz@gmail.com>
2017-11-03 15:11:46 +0100

    init commit for gradle and spring boot with web and other dependencies

---
+++ src/main/java/deboss/reminder/ReminderApplication.java
@@ -0,0 +1,12 @@
+package deboss.reminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderApplication.class, args);
+	}
+}
